5e07c67af5f928fee317cc40f8c1805d2a602ffc
ambiesoft <xFarv@THEXP.mshome.net>
2019-07-06 21:47:16 +0900

    install explanation

--- tango/setresult.php
+++ tango/setresult.php
@@ -36,6 +36,13 @@ if($kindstring=='normal') {
 	die('Illegal Kind');
 }
 
+// To install google-api run the following
+// php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+// php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+// php composer-setup.php
+// php -r "unlink('composer-setup.php');"
+// ./composer.phar require google/apiclient:"^2.0"
+
 // validate token and get userid
 $userid = '';
 $CLIENT_ID="330872316416-lvi3ta181uma742srekov7nr7kcevfdc.apps.googleusercontent.com";
